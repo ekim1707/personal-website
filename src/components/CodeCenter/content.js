551f1f1329586abac9f47ab71efe9b91063119ed
Eugene Kim <ekim1707@gmail.com>
2021-11-11 21:43:16 -0500

    updated code center

--- src/components/CodeCenter/content.js
+++ src/components/CodeCenter/content.js
@@ -8,6 +8,92 @@ export const content = {
 
 export const leetcodeContent = [
     {
+        problem: "Paint a Skyline",
+        href: "maximum-units-on-a-truck",
+        description: `
+        Paint a skyline on a wall
+        Rectangular building arranged in a line
+        Buildings are same width but may have diff heights
+        Skyline shape is given as array A whose elements specify heights
+    
+        Paint the skyline with continuous horizontal brushstrokes
+        Every stroke is one unit high and arbitrarily wide
+        Calculate min number of horizontal strokes needed
+    
+        Write function such that given non-empty array A with N integers
+        Returns minimum number of horizontal brushstrokes needed to paint shape by array
+    
+
+        Constraints:
+        
+        N is integer within range [1..100,000]
+        Each element of array A is integer within range [1..1,000,000,000]
+        `,
+        solution: `
+        function solution(A) {
+            // write your code in JavaScript (Node.js 8.9.4)
+        
+            // if every element in the array includes a line number, the line will be continuous
+            // if the line is not continuous, then there will be breaks every time an element of the array goes below that line number
+            // how to do these calculations without having to loop through the entire array for every single line number checked
+        
+            // declarations:
+            let previousLineHeight = 0;
+            let runningCount = 0;
+        
+            // loop through array
+            for (let x = 0; x < A.length; x++) {
+                if (A[x] > previousLineHeight) {
+                    runningCount += (A[x] - previousLineHeight);
+                }
+                previousLineHeight = A[x];
+            }
+        
+            return runningCount;
+        }
+        `,
+        cardContentArray: [
+            { content: `Every new brushstroke comes when the next array height is greater than the one before it, and the number of new brushstrokes is equal to the next array height minus the previous array height. Time Complexity: O(n) for the for loop.` }
+        ]
+    },
+    {
+        problem: "Find Greatest Integer Sibling (Easy)",
+        href: "",
+        description: `
+        Two non-negative integers are called siblings if they are obtained from rearranging the digits of their decimal representations: e.g. 123 and 213
+        A set consisting of non-negative integer N and all siblings is called family of N: e.g. family of 553 comporises of 355, 535, and 553
+        Given non-negative integer N, return largest number in family of N
+        Function should return -1 if result exceeds 100,000,000
+        N = 213 should return 321
+        N = 553 should return 553
+
+        
+        Constraints:
+        
+        N is integer in range [0..2,147,483,647]
+        `,
+        solution: `
+        function solution(N) {
+            // write your code in JavaScript (Node.js 8.9.4)
+        
+            // declarations
+            const splitNArray = N.toString().split("").map(val => parseInt(val));
+        
+            // handle N > 100,000,000
+            return (
+                N > 100000000 ? 
+                    -1 :
+                        Number(splitNArray.sort(function(a, b) {
+                            return b - a;
+                        }).join(""))
+            );
+        }
+        `,
+        cardContentArray: [
+            { content: `The largest sibling (number) for every family of N will always be the largest single-digit integers comprising it arranged in order of greatest to least and then concatenated. Time Complexity: at worst, O(n log n) for the sort function.` }
+        ]
+    },
+    {
         problem: "1710. Maximum Units on a Truck (Easy)",
         href: "maximum-units-on-a-truck",
         description: `
